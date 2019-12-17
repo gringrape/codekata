ecfb07634a13512f0a3ead0abc29592bdb5d33ff
gringrape <gringrape200@gmail.com>
2019-12-17 17:50:34 +0900

    2019-12-17 품새 연습
    PrimeNumbers 연습

--- personal training/java/src/main/java/primeFactors/PrimeFactors.java
+++ personal training/java/src/main/java/primeFactors/PrimeFactors.java
@@ -2,6 +2,8 @@ package primeFactors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class PrimeFactors {
 
@@ -9,19 +11,31 @@ public class PrimeFactors {
 
         List<Integer> primes = new ArrayList<>();
 
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                primes.add(i);
-                primes.add(n / i);
-                break;
-            }
-        }
+        OptionalInt divider = IntStream.range(2, n)
+                .filter(i -> n % i == 0)
+                .findFirst();
 
-        if (n != 1 && primes.size() == 0)
+        divider.ifPresent(i -> {
+            primes.add(i);
+            if (isPrime(n / i))
+                primes.add(n / i);
+            else
+                primes.addAll(of(n / i));
+        });
+
+        if (n != 1 && !divider.isPresent())
             primes.add(n);
 
         return primes;
+    }
 
+    public static boolean isPrime(Integer number) {
+
+        int[] ints = IntStream.range(2, number)
+                .filter(i -> number % i == 0)
+                .toArray();
+
+        return ints.length == 0;
     }
 
 }
--- personal training/java/src/test/java/primeFactors/PrimeFactorsTests.java
+++ personal training/java/src/test/java/primeFactors/PrimeFactorsTests.java
@@ -4,14 +4,16 @@ import org.junit.Test;
 import primeFactors.utils.ImmutableList;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static primeFactors.PrimeFactors.isPrime;
 import static primeFactors.PrimeFactors.of;
+import static primeFactors.utils.ImmutableList.of;
 
 public class PrimeFactorsTests {
 
     @Test
     public void of_1isNone() {
         assertThat(of(1))
-                .isEqualTo(ImmutableList.of());
+                .isEqualTo(of());
     }
 
     @Test
@@ -29,7 +31,60 @@ public class PrimeFactorsTests {
     @Test
     public void of4_is22() {
         assertThat(of(4))
-                .isEqualTo(ImmutableList.of(2, 2));
+                .isEqualTo(of(2, 2));
     }
+
+    @Test
+    public void of6_is23() {
+        assertThat(of(6))
+                .isEqualTo(of(2, 3));
+    }
+
+    @Test
+    public void of8_is222() {
+        assertThat(of(8))
+                .isEqualTo(of(2, 2, 2));
+    }
+
+    @Test
+    public void of10_is25() {
+        assertThat(of(10))
+                .isEqualTo(of(2, 5));
+    }
+    
+    @Test
+    public void of100_is2255() {
+        assertThat(of(100))
+                .isEqualTo(ImmutableList.of(2, 2, 5, 5));
+
+    }
+
+    @Test
+    public void isPrime7_isTrue() {
+        assertThat(isPrime(7))
+                .isEqualTo(true);
+    }
+
+    @Test
+    public void isPrime8_isFalse() {
+        assertThat(isPrime(8))
+                .isEqualTo(false);
+    }
+
+    @Test
+    public void isPrimePrimeNumbersLessThan20_isAllTrue() {
+        int[] primeNumbers = {2, 3, 5, 7, 11, 13, 17, 19};
+
+        for (int num : primeNumbers) {
+
+            assertThat(isPrime(num))
+                    .isEqualTo(true);
+
+        }
+    }
+
+    // TODO: 2019-12-17
+    // - 현재 숫자를 두개의 약수로 쪼개는 것 까지는 수행했다.
+    // - 두개의 약수를 세개의 약수로 나누고 싶은 경우에는 위의 과정에서 나누어진 약수 중 하나를 같은 과정으로 나눌 필요가 있다.
 
 }
