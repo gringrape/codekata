fcb1c7495dfaca7a3dfcf5408600811872e42802
gringrape <gringrape200@gmail.com>
2019-12-18 09:20:43 +0900

    2019-12-18 아침 수련
    자바 스크립트 테스트 환경 구축(Jest)
    반복되는 테스트 코드를 snippet으로

---
+++ personal training/javascript/js/primeNumbers.js
@@ -0,0 +1,13 @@
+// 목적
+// - 인수분해 하는 클래스를 선언 한다.
+// - 함수를 property로 넣어주고 export 해준다.
+
+let primeNumbers = {
+    of(num) {
+        if (num === 1) {
+            return null
+        }        
+    }
+}
+
+module.exports = primeNumbers
---
+++ personal training/javascript/js/primeNumbers.test.js
@@ -0,0 +1,10 @@
+const primeNumbers = require('./primeNumbers')
+
+test('of1_isNone', () => {
+   expect(primeNumbers.of(1)).toBe(null);
+})
+
+test('of2_is2', () => {
+   expect(primeNumber.of(2)).toBe(2);
+})
+
