752fd52290b27109693d0c7a6b0006a306a697d9
gringrape <gringrape200@gmail.com>
2019-12-22 09:40:47 +0900

    2019-12-22 아침 수련
    - jest 를 이용한 테스트 연습 -> toEqual array 원소 비교 -> jest 공식 문서 matchers 참고
    - 소인수 분해 1차 코딩 -> 재귀를 이용한 방법 사용 -> 지나친 중첩 개선 필요

--- personal training/javascript/js/primeNumbers.js
+++ personal training/javascript/js/primeNumbers.js
@@ -4,9 +4,30 @@
 
 let primeNumbers = {
     of(num) {
-        if (num === 1) {
-            return null
-        }        
+        if(num < 2) return []
+
+        let result = []
+        if(this.isPrime(num)) result.push(num)
+        else {
+            for (var i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    result.push(i)
+                    for (let item of this.of(num / i)) {
+                        result.push(item)
+                    }
+                    break
+                }
+            }
+        }
+        return result
+    },
+    isPrime(num) {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                return false
+            }
+        }    
+        return true   
     }
 }
 
